dae58f76d892bf73c468f4c05cdc204bdfdd023b
robinvh85 <robinvh85@gmail.com>
2020-08-22 10:45:46 +0700

    Setup project

---
+++ src/components/App/index.tsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+// import { Router, Route, Switch } from 'react-router-dom';
+
+// import history from 'browserHistory';
+import 'assets/styles/App.scss';
+
+const App = () => {
+  return (
+    <Fragment>
+      <div>
+        APP
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
